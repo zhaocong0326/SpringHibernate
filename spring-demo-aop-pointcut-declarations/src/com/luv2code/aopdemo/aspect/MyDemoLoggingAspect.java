1fb95a62f3c806816732a1158d663bf0acca524a
user <user1@userdeMacBook-Air.local>
2019-01-12 23:05:52 +0800

    “test”

--- spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,20 +2,23 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 
-	// this is where we add all of our related advices for logging
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
 	
-	// let's start with an @Before advice
-	
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
-		
 		System.out.println("\n======>>> Excuting @Before advice on method");
+	}
 	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====>>> Performing API analytics");
 	}
 }
