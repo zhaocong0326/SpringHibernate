c44d5e51ee9918fec985560b58953592e22fbec6
user <user1@userdeMacBook-Air.local>
2019-01-17 03:41:44 +0800

    “test”

---
+++ spring-demo-aop-zz-around/src/com/luv2code/aopdemo/service/TrafficFortuneService.java
@@ -0,0 +1,25 @@
+package com.luv2code.aopdemo.service;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrafficFortuneService {
+
+	public String getFortune() {
+		
+		// simulate a delay
+		
+		try {
+			
+			TimeUnit.SECONDS.sleep(5);
+			
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//return a fortune
+		return "Exception heavy traffic this morning";
+	}
+}
